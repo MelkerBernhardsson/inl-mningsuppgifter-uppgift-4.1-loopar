070c03995fdf0ee84190e9d2cbb6219673bf950e
Pingvu <nhlpool27@gmail.com>
2023-03-03 08:37:16 +0100

    Add project files.

---
+++ inlämningsuppgifter uppgift 4.1 loopar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace loopar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           for(int x =10; x<=30; x++)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("\n");
+            for (int y = 200; y>=180; y--)
+            {
+                Console.WriteLine(y);
+            }
+            Console.WriteLine("\n");
+            for (int z = 1000; z <= 1400; z += 50) 
+            {
+                Console.WriteLine(z);
+            }
+        }
+    }
+}
